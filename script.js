973cf018bb0f0bf5db4f5a0320056795732b9351
sulyman002 <oyedelesulaiman@gmail.com>
2025-06-10 14:06:04 +0100

    done with the project

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-const mailUs = document.getElementById('mailUs').addEventListener('submit', function(avoidError){
-    avoidError.preventDefault();
-} );
 
 
 
-mailUs.forEach(function(mail){
-    mail.classList.remove('error');
+document.getElementById('mailButton').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const emailInput = document.getElementById('emailInput');
+  const errorMessage = document.getElementById('errorMessage');
+  const email = emailInput.value.trim();
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!emailRegex.test(email)) {
+    errorMessage.style.display = 'block';
+    emailInput.classList.add('error');
+  } else {
+    errorMessage.style.display = 'none';
+    emailInput.classList.remove('error');
+    '';
+
+  }
 });
-
-let thereIsError = false;
-const emailRedex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-if(!emailRedex.test(mailUs.value.trim())) {
-
-}
