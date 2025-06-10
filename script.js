f0809a047fc9ef6ef891926866c13b295bb3345e
sulyman002 <oyedelesulaiman@gmail.com>
2025-06-10 12:31:37 +0100

    i want to work on the javascript code of the project

---
+++ script.js
@@ -0,0 +1,16 @@
+const mailUs = document.getElementById('mailUs').addEventListener('submit', function(avoidError){
+    avoidError.preventDefault();
+} );
+
+
+
+mailUs.forEach(function(mail){
+    mail.classList.remove('error');
+});
+
+let thereIsError = false;
+const emailRedex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+if(!emailRedex.test(mailUs.value.trim())) {
+
+}
